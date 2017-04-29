0071b752a9d489043cd5a7276cd1e538f5da7e55
Jonathan Zhou <zhouronghui30@163.com>
2017-04-29 23:58:38 +0800

    initial of the ModelMetaclass class in orm.py

--- www/transwarp/orm.py
+++ www/transwarp/orm.py
@@ -1,5 +1,7 @@
 # !/usr/bin/env python
 # encoding=utf-8
+import logging
+
 
 class Model(dict):
     __metaclass__ = ModelMetaclass
@@ -15,3 +17,29 @@ class Model(dict):
 
     def __setattr__(self, key, value):
         self[key] = value
+
+
+class ModelMetaclass(type):
+    """
+    Metaclass for model objects.
+    """
+
+    def __new__(cls, name, bases, attrs):
+        # skip for the Model class:
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+
+        # store all subclasses info:
+        if not hasattr(cls, 'subclasses'):
+            cls.subclasses = {}
+        if name not in cls.subclasses:
+            cls.subclasses[name] = name
+        else:
+            logging.warning('Redefine class: %s' % name)
+
+        logging.info('Scan ORMapping %s...' % name)
+        mapping = dict()
+        primary_key = None
+        for k, v in attrs.iteritems():
+            if isinstance(v, Field):
+                pass
