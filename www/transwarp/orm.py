406e544c6012bab1e408d53e8bc3c9dfb4768e35
Jonathan Zhou <zhouronghui30@163.com>
2017-04-29 23:49:50 +0800

    initial of the Model class in orm.py

--- www/transwarp/orm.py
+++ www/transwarp/orm.py
@@ -1,3 +1,17 @@
 # !/usr/bin/env python
 # encoding=utf-8
 
+class Model(dict):
+    __metaclass__ = ModelMetaclass
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"Dict object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
