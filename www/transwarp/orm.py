44fd2d3860e8658aee9620dec1d36a65cfc57081
Jonathan Zhou <zhouronghui30@163.com>
2017-04-30 23:31:47 +0800

    implement of 'insert' function in Model class

--- www/transwarp/orm.py
+++ www/transwarp/orm.py
@@ -116,6 +116,61 @@ def _gen_sql(table_name, mappings):
     return '\n'.join(sql)
 
 
+class ModelMetaclass(type):
+    """
+    Metaclass for model objects.
+    """
+
+    def __new__(cls, name, bases, attrs):
+        # skip for the Model class:
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+
+        # store all subclasses info:
+        if not hasattr(cls, 'subclasses'):
+            cls.subclasses = {}
+        if name not in cls.subclasses:
+            cls.subclasses[name] = name
+        else:
+            logging.warning('Redefine class: %s' % name)
+
+        logging.info('Scan ORMapping %s...' % name)
+        mappings = dict()
+        primary_key = None
+        for k, v in attrs.iteritems():
+            if isinstance(v, Field):
+                if not v.name:
+                    v.name = k
+                logging.info('Found mapping: %s => %s' % (k, v))
+
+                # check duplicate primary key:
+                if v.primary_key:
+                    if primary_key:
+                        raise TypeError('cannot define more that 1 primary ket in class: %s' % name)
+                    if v.updateable:
+                        logging.warning('NOTE: change primary key to non-updateable.')
+                        v.updateable = False
+                    if v.nullable:
+                        logging.warning('NOTE: change primary key to non-nullable')
+                        v.nullable = False
+                    primary_key = v
+                mappings[k] = v
+        # check exist of primary key:
+        if not primary_key:
+            raise TypeError('Primary key not define in class: %s' % name)
+        for k in mappings.iterkeys():
+            attrs.pop(k)
+        if '__table__' not in attrs:
+            attrs['__talbe__'] = name.lower()
+        attrs['__mapping__'] = mappings
+        attrs['__primary_key__'] = primary_key
+        attrs['__sql__'] = lambda self: _gen_sql(attrs['__table__'], mappings)
+        for trigger in _triggers:
+            if trigger not in attrs:
+                attrs[trigger] = None
+        return type.__new__(cls, name, bases, attrs)
+
+
 class Model(dict):
     __metaclass__ = ModelMetaclass
 
@@ -193,57 +248,13 @@ class Model(dict):
         db.update('delete from `%s` where `%s` = ?' % (self.__table__, pk), *args)
         return self
 
-
-class ModelMetaclass(type):
-    """
-    Metaclass for model objects.
-    """
-
-    def __new__(cls, name, bases, attrs):
-        # skip for the Model class:
-        if name == 'Model':
-            return type.__new__(cls, name, bases, attrs)
-
-        # store all subclasses info:
-        if not hasattr(cls, 'subclasses'):
-            cls.subclasses = {}
-        if name not in cls.subclasses:
-            cls.subclasses[name] = name
-        else:
-            logging.warning('Redefine class: %s' % name)
-
-        logging.info('Scan ORMapping %s...' % name)
-        mappings = dict()
-        primary_key = None
-        for k, v in attrs.iteritems():
-            if isinstance(v, Field):
-                if not v.name:
-                    v.name = k
-                logging.info('Found mapping: %s => %s' % (k, v))
-
-                # check duplicate primary key:
-                if v.primary_key:
-                    if primary_key:
-                        raise TypeError('cannot define more that 1 primary ket in class: %s' % name)
-                    if v.updateable:
-                        logging.warning('NOTE: change primary key to non-updateable.')
-                        v.updateable = False
-                    if v.nullable:
-                        logging.warning('NOTE: change primary key to non-nullable')
-                        v.nullable = False
-                    primary_key = v
-                mappings[k] = v
-        # check exist of primary key:
-        if not primary_key:
-            raise TypeError('Primary key not define in class: %s' % name)
-        for k in mappings.iterkeys():
-            attrs.pop(k)
-        if '__table__' not in attrs:
-            attrs['__talbe__'] = name.lower()
-        attrs['__mapping__'] = mappings
-        attrs['__primary_key__'] = primary_key
-        attrs['__sql__'] = lambda self: _gen_sql(attrs['__table__'], mappings)
-        for trigger in _triggers:
-            if trigger not in attrs:
-                attrs[trigger] = None
-        return type.__new__(cls, name, bases, attrs)
+    def insert(self):
+        self.pre_insert and self.pre_insert()
+        params = {}
+        for k, v in self.__mappings__.iteritems():
+            if v.insertable:
+                if not hasattr(self, k):
+                    setattr(self, k, v.default)
+                params[v.name] = getattr(self, k)
+        db.insert('%s' % self.__table__, **params)
+        return self
