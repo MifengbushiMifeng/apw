d76782f576be8d55bf6da65609281b931091ce4e
Jonathan Zhou <zhouronghui30@163.com>
2017-04-30 13:26:28 +0800

    implement of ModelMetaclass class in orm.py

--- www/transwarp/orm.py
+++ www/transwarp/orm.py
@@ -90,10 +90,10 @@ class VersionField(Field):
         super(VersionField, self).__init__(name=name, default=0, ddl='bigint')
 
 
-_tirggers = frozenset(['pre_insert', 'pre_update', 'pre_delete'])
+_triggers = frozenset(['pre_insert', 'pre_update', 'pre_delete'])
 
 
-def gen_sql(table_name, mappings):
+def _gen_sql(table_name, mappings):
     """
     Generate sql by using the table_name and mappings
     :param table_name: the DB table name
@@ -150,8 +150,37 @@ class ModelMetaclass(type):
             logging.warning('Redefine class: %s' % name)
 
         logging.info('Scan ORMapping %s...' % name)
-        mapping = dict()
+        mappings = dict()
         primary_key = None
         for k, v in attrs.iteritems():
             if isinstance(v, Field):
-                pass
+                if not v.name:
+                    v.name = k
+                logging.info('Found mapping: %s => %s' % (k, v))
+
+                # check duplicate primary key:
+                if v.primary_key:
+                    if primary_key:
+                        raise TypeError('cannot define more that 1 primary ket in class: %s' % name)
+                    if v.updateable:
+                        logging.warning('NOTE: change primary key to non-updateable.')
+                        v.updateable = False
+                    if v.nullable:
+                        logging.warning('NOTE: change primary key to non-nullable')
+                        v.nullable = False
+                    primary_key = v
+                mappings[k] = v
+        # check exist of primary key:
+        if not primary_key:
+            raise TypeError('Primary key not define in class: %s' % name)
+        for k in mappings.iterkeys():
+            attrs.pop(k)
+        if '__table__' not in attrs:
+            attrs['__talbe__'] = name.lower()
+        attrs['__mapping__'] = mappings
+        attrs['__primary_key__'] = primary_key
+        attrs['__sql__'] = lambda self: _gen_sql(attrs['__table__'], mappings)
+        for trigger in _triggers:
+            if trigger not in attrs:
+                attrs[trigger] = None
+        return type.__new__(cls, name, bases, attrs)
