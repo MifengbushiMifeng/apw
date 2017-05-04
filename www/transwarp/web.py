c1987961658d9c226f6797fcf544df024849887e
Jonathan <zhouronghui30@163.com>
2017-05-04 16:56:32 +0800

    implement of Response class

--- www/transwarp/web.py
+++ www/transwarp/web.py
@@ -600,3 +600,138 @@ class Request(object):
 UTC_0 = UTC('+00:00')
 
 
+class Response(object):
+    def __init__(self):
+        self._status = '200 OK'
+        self._headers = {'CONTENT_TYPE': 'text/html; charset=utf-8'}
+
+    @property
+    def headers(self):
+        L = [(_RESPONSE_HEADER_DICT.get(k, k), v) for k, v in self._headers.iteritems()]
+        if hasattr(self, '_cookies'):
+            for v in self._cookies.itervalues():
+                L.append(('Set-Cookies', v))
+            L.append(_HEADER_X_POWERED_BY)
+        return L
+
+    def header(self, name):
+        """
+        Get header by name, case-insensitive
+        :param name: 
+        :return: 
+        """
+        key = name.upper()
+        if key not in _RESPONSE_HEADER_DICT:
+            key = name
+        return self._headers.get(key)
+
+    def unset_header(self, name):
+        """
+        Unset header by name and value.
+        :param name: 
+        :return: 
+        """
+        key = name.upper()
+        if key not in _RESPONSE_HEADER_DICT:
+            key = name
+        if key in self._headers:
+            del self._headers[key]
+
+    def set_header(self, name, value):
+        """
+        Set header by name and value
+        :param name: 
+        :param value: 
+        :return: 
+        """
+        key = name.upper()
+        if key not in _RESPONSE_HEADER_DICT:
+            key = name
+        self._headers[key] = _to_str(value)
+
+    @property
+    def content_type(self):
+        return self.header('CONTENT-TYPE')
+
+    @content_type.setter
+    def content_type(self, value):
+        if value:
+            self.set_header('CONTENT-TYPE', value)
+        else:
+            self.unset_header('CONTENT-TYPE')
+
+    @property
+    def content_length(self):
+        return self.header('CONTENT-LENGTH')
+
+    @content_length.setter
+    def content_length(self, value):
+        self.set_header('CONTENT-LENGTH', str(value))
+
+    def delete_cookie(self, name):
+        self.set_cookie(name, '__deleted__', expires=0)
+
+    def set_cookie(self, name, value, max_age=None, expires=None,
+                   path='/', domain=None, secure=False, http_only=True):
+
+        """
+        Set a cookie
+        :param name: the cookie name
+        :param max_age: optional, seconds of cookies's max age.
+        :param expires: optional, unix timestamp, datetime or date object that
+                indicate on absolute time of the expiration time of cookie.
+                Note that if expires specified, the max_age will be ignored.
+        :param path: the cookie path, default is '/'
+        :param domain: the cookie domain, default is None
+        :param secure: if the cookie is secure, default is False
+        :param http_only: if the cookie is http only, default is False
+        :return: the cookie
+        """
+        if not hasattr(self, '_cookies'):
+            self._cookies = {}
+        L = ['%s=%s' % (_quote(name), _quote(value))]
+        if expires is not None:
+            if isinstance(expires, (float, int, long)):
+                L.append('Expires=%s' % datetime.datetime.
+                         fromtimestamp(expires, UTC_0).strftime('%a, %d-%b-%Y %H:%M:%S GMT'))
+            if isinstance(expires, (datetime.date, datetime.datetime)):
+                L.append('Expires=%s' % expires.astimezone(UTC_0).strftime('%a, %d-%b-%Y %H:%M:%S GMT'))
+        elif isinstance((max_age, (int, long))):
+            L.append('Max-Age=%d' % max_age)
+        L.append('Path=%s' % path)
+        if domain:
+            L.append('Domain=%s' % domain)
+
+    def unset_cookie(self, name):
+        if hasattr(self, '_cookies'):
+            if name in self._cookies:
+                del self._cookies[name]
+
+    @property
+    def status_code(self):
+        return int(self._status[:3])
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        if isinstance(value, (int, long)):
+            if 100 <= value <= 999:
+                st = _RESPONSE_STATUSES.get(value, '')
+                if st:
+                    self._status = '%d %s' % (value, st)
+                else:
+                    self._status = str(value)
+            else:
+                raise ValueError('Bad response code: %d' % value)
+        elif isinstance(value, basestring):
+            if isinstance(value, unicode):
+                value = value.encode('utf-8')
+            if _RE_RESPONSE_STATUS.match(value):
+                self._status = value
+            else:
+                raise ValueError('Bad response code: %s' % value)
+        else:
+            raise TypeError('Bad type of response code.')
