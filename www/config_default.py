cea15979468cf0e82aa72020d57ad39adf662032
Jonathan Zhou <zhouronghui30@163.com>
2017-05-05 22:54:55 +0800

    implement of config...

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# encoding=utf-8
+
+"""
+Default configurations.
+"""
+__author__ = 'Jonathan Zhou'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+
+}
---
+++ www/config_override.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# encoding=utf-8
+
+'''
+Override configurations.
+'''
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
