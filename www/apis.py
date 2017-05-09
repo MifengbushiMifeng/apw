128955a36784492bd56b03cc3221342f8d56f134
Jonathan <zhouronghui30@163.com>
2017-05-09 16:54:57 +0800

    implement of the apis.py

--- www/apis.py
+++ www/apis.py
@@ -19,12 +19,46 @@ class Page(object):
     """
 
     def __init__(self, item_count, page_index=1, page_size=15):
+        """
+        Init pagination.
+        :param item_count: 
+        :param page_index: 
+        :param page_size: 
+        >>> p1 = Page(100,1)
+        >>> p1.page_count
+        7
+        """
         self.item_count = item_count
+        self.page_size = page_size
         self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0)
+        if (item_count == 0) or (page_index < 1) or (page_index > self.page_count):
+            self.offset = 0
+            self.limit = 0
+            self.page_index = 1
+        else:
+            self.page_index = page_index
+            self.offset = self.page_size * (page_index - 1)
+            self.limit = self.page_size
+        self.has_next = self.page_index < self.page_count
+        self.has_previous = self.page_index > 1
+
+    def __str__(self):
+        return 'item_count: %s, page_count: %s, page_index: %s, page_size: %s, offset: %s, limit: %s' % (
+            self.item_count, self.page_count, self.page_index, self.page_size, self.offset, self.limit)
+
+    __repr__ = __str__
 
 
-def _dump(obj, Page):
-    pass
+def _dump(obj):
+    if isinstance(obj, Page):
+        return {
+            'page_index': obj.page_index,
+            'page_count': obj.page_count,
+            'item_count': obj.item_count,
+            'has_next': obj.has_next,
+            'has_previous': obj.has_previous
+        }
+    raise TypeError('%s is not JSON serializable' % obj)
 
 
 def dumps(obj):
@@ -80,6 +114,19 @@ def api(func):
     @functools.wraps(func)
     def _wrapper(*args, **kw):
         try:
-            pass
-        except:
-            pass
+            r = dumps(func(*args, **kw))
+        except APIError, e:
+            r = json.dumps(dict(error=e.error, data=e.data, message=e.message))
+        except Exception, e:
+            logging.exception(e)
+            r = json.dumps(dict(error='internal error', data=e.__class__.__name__, message=e.message))
+        ctx.response.content_type = 'application/json'
+        return r
+
+    return _wrapper
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
